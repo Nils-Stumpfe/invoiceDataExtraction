8644f772f5b770ec32b119fc1fdbdaebbf39203b
Nils Stumpfe <nstumpfe@stud.hs-heilbronn.de>
2025-01-29 14:20:37 +0100

    if statem for pdf or txt, impl pdf parser

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-const { readFileSync, existsSync } = require("fs");
+const fs = require("fs");
+const path = require("node:path");
+const pdfparse = require("pdf-parse");
 
 // Argument aus der Kommandozeile holen
 const filePath = process.argv[2];
@@ -10,15 +12,33 @@ if (!filePath) {
 }
 
 // Prüfen, ob die Datei existiert
-if (!existsSync(filePath)) {
+if (!fs.existsSync(filePath)) {
     console.error(`Fehler: Die Datei "${filePath}" existiert nicht.`);
     process.exit(1);
 }
 
-try {
-    // Dateiinhalt lesen
-    const txt = readFileSync(filePath, "utf-8"); // "utf-8" sorgt für die richtige Codierung
-    console.log("Datei erfolgreich gelesen:\n", txt);
-} catch (error) {
-    console.error("Fehler beim Lesen der Datei:", error.message);
+// Prüfen, ob es sich um .txt oder .pdf handelt
+if (path.extname(filePath) == ".txt") { //ist eine .txt Datei
+
+    console.log("ist txt")
+
+} else { //ist eine .pdf Datei
+
+    try {
+        // Dateiinhalt lesen
+        const pdf = fs.readFileSync(filePath);
+    
+        pdfparse(pdf).then(function (data) {
+            console.log(data.text)
+        })
+    
+    
+    
+        //console.log("Datei erfolgreich gelesen:\n", pdf);
+    } catch (error) {
+        console.error("Fehler beim Lesen der Datei:", error.message);
+    }
+        
+
 }
+
