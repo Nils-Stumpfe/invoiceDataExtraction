16f4292a54cfd1489438cc6f3b12ef712852b0d0
Nils Stumpfe <nstumpfe@stud.hs-heilbronn.de>
2025-01-29 11:02:17 +0100

    added additionals Args

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const { readFile, readFileSync } = require ("fs");
 const txt = readFileSync("./test_files/Rechnung_Level_0.txt", "utf8");
 
+if (process.argv.length === 2) {
+    console.error('Expected PATH_TO_PDF as an argument!');
+    process.exit(1);
+  }
 
-console.log(txt)
+
+
+ console.log(txt)
